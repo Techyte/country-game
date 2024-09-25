70c7723108a3af2732e655bd5712dc01d00fdbc5
Techyte <owinterh@icloud.com>
2024-09-25 20:26:34 +1000

    Major progress on getting my head around steamworks lol

--- country game/Assets/Scripts/CountryGame/Multiplayer/LobbyManager.cs
+++ country game/Assets/Scripts/CountryGame/Multiplayer/LobbyManager.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections.Generic;
 using Steamworks;
+using TMPro;
+using UnityEngine.UI;
 
 namespace CountryGame.Multiplayer
 {
@@ -32,46 +35,189 @@ namespace CountryGame.Multiplayer
         protected Callback<LobbyCreated_t> LobbyCreated;
         protected Callback<GameLobbyJoinRequested_t> GameLobbyJoinRequested;
         protected Callback<LobbyEnter_t> LobbyEnter;
+        protected Callback<AvatarImageLoaded_t> AvatarImageLoaded;
+        protected Callback<LobbyChatUpdate_t> LobbyChatUpdate;
         
         private const string HostAddressKey = "HostAddress";
-        private CSteamID lobbyId;
+        public CSteamID lobbyId;
+
+        [SerializeField] private TextMeshProUGUI idText;
+        [SerializeField] private TMP_InputField idInput;
+        [SerializeField] private TextMeshProUGUI lobbyName;
+        [SerializeField] private Transform lobbyClientParent;
+        [SerializeField] private GameObject lobbyClientPrefab;
+        [SerializeField] private Button beginButton;
 
         private void Awake()
         {
             Instance = this;
         }
-        //
-        // private void Start()
-        // {
-        //     if (!SteamManager.Initialized)
-        //     {
-        //         Debug.LogError("Steam is not initialized!");
-        //         return;
-        //     }
-        //     
-        //     LobbyCreated = Callback<LobbyCreated_t>.Create(OnLobbyCreated); 
-        //     GameLobbyJoinRequested = Callback<GameLobbyJoinRequested_t>.Create(OnGameLobbyJoinRequested);
-        //     LobbyEnter = Callback<LobbyEnter_t>.Create(OnLobbyEnter);
-        // }
-        //
-        // internal void CreateLobby()
-        // {
-        //     SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, 5);
-        // }
-        //
-        // private void OnLobbyCreated(LobbyCreated_t callback)
-        // {
-        //     if (callback.m_eResult != EResult.k_EResultOK)
-        //     {
-        //         UIManager.Singleton.LobbyCreationFailed();
-        //         return;
-        //     }
-        //
-        //     lobbyId = new CSteamID(callback.m_ulSteamIDLobby);
-        //     UIManager.Singleton.LobbyCreationSucceeded(callback.m_ulSteamIDLobby);
-        //
-        //     NetworkManager.Instance.Server.Start(0, 5, NetworkManager.PlayerHostedDemoMessageHandlerGroupId);
-        //     NetworkManager.Instance.Client.Connect("127.0.0.1", messageHandlerGroupId: NetworkManager.PlayerHostedDemoMessageHandlerGroupId);
-        // }
+        
+        private void Start()
+        {
+            if (!SteamManager.Initialized)
+            {
+                Debug.LogError("Steam is not initialized!");
+                return;
+            }
+            
+            LobbyCreated = Callback<LobbyCreated_t>.Create(OnLobbyCreated);
+            GameLobbyJoinRequested = Callback<GameLobbyJoinRequested_t>.Create(OnLobbyJoinRequested);
+            LobbyEnter = Callback<LobbyEnter_t>.Create(OnLobbyEnter);
+            AvatarImageLoaded = Callback<AvatarImageLoaded_t>.Create(OnAvatarImageLoaded);
+            LobbyChatUpdate = Callback<LobbyChatUpdate_t>.Create(OnLobbyChatUpdate);
+        }
+        
+        public void CreateLobby()
+        {
+            SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, 5);
+            Debug.Log("Requested Lobby creation");
+        }
+        
+        private void OnLobbyCreated(LobbyCreated_t callback)
+        {
+            if (callback.m_eResult != EResult.k_EResultOK)
+            {
+                return;
+            }
+            
+            Debug.Log("Lobby created success");
+        
+            lobbyId = new CSteamID(callback.m_ulSteamIDLobby);
+        
+            NetworkManager.Instance.Server.Start(0, 5, NetworkManager.PlayerHostedDemoMessageHandlerGroupId);
+            NetworkManager.Instance.Client.Connect("127.0.0.1", messageHandlerGroupId: NetworkManager.PlayerHostedDemoMessageHandlerGroupId);
+            
+            idText.text = $"Lobby ID: {callback.m_ulSteamIDLobby.ToString()}";
+            SteamMatchmaking.SetLobbyData((CSteamID)callback.m_ulSteamIDLobby, "name",
+                SteamFriends.GetPersonaName() + "s Lobby");
+            
+            lobbyName.text = SteamFriends.GetPersonaName() + "s Lobby";
+            
+            DisplayLobbyMembers(lobbyId);
+        }
+
+        private void OnLobbyEnter(LobbyEnter_t callback)
+        {
+            if (NetworkManager.Instance.Server.IsRunning)
+                return;
+            
+            Debug.Log("Entered Lobby");
+
+            lobbyId = new CSteamID(callback.m_ulSteamIDLobby);
+            CSteamID hostId = SteamMatchmaking.GetLobbyOwner(lobbyId);
+            
+            NetworkManager.Instance.Client.Connect(hostId.ToString(), messageHandlerGroupId: NetworkManager.PlayerHostedDemoMessageHandlerGroupId);
+            idText.text = $"Lobby ID: {callback.m_ulSteamIDLobby.ToString()}";
+            lobbyName.text = SteamMatchmaking.GetLobbyData(lobbyId, "name");
+            
+            DisplayLobbyMembers(lobbyId);
+        }
+
+        private Dictionary<CSteamID, GameObject> currentLobbyClients = new Dictionary<CSteamID, GameObject>();
+        public void DisplayLobbyMembers(CSteamID lobbyId)
+        {
+            foreach (var obj in currentLobbyClients.Values)
+            {
+                Destroy(obj);
+            }
+            
+            currentLobbyClients.Clear();
+            
+            int numOfMembers = SteamMatchmaking.GetNumLobbyMembers(lobbyId);
+
+            for (int i = 0; i < numOfMembers; i++)
+            {
+                GameObject obj = Instantiate(lobbyClientPrefab, lobbyClientParent);
+                
+                CSteamID userId = SteamMatchmaking.GetLobbyMemberByIndex(lobbyId, i);
+                string username = SteamFriends.GetFriendPersonaName(userId);
+
+                obj.GetComponentInChildren<TextMeshProUGUI>().text = username;
+                
+                currentLobbyClients.Add(userId, obj);
+                
+                int imageId = SteamFriends.GetLargeFriendAvatar(userId);
+
+                if (imageId == -1)
+                {
+                    return;
+                }
+
+                Texture2D texture = GetSteamImageAsTexture(imageId);
+
+                obj.GetComponentInChildren<Image>().sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+            }
+        }
+        
+        protected void OnAvatarImageLoaded(AvatarImageLoaded_t callback)
+        {
+            if (callback.m_steamID == SteamUser.GetSteamID())
+            {
+                Texture2D texture = GetSteamImageAsTexture(SteamFriends.GetLargeFriendAvatar(callback.m_steamID));
+
+                if (currentLobbyClients.TryGetValue(callback.m_steamID, out GameObject obj))
+                {
+                    obj.GetComponentInChildren<Image>().sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+                }
+            }
+        }
+        
+        private Texture2D GetSteamImageAsTexture(int iImage)
+        {
+            Texture2D texture = null;
+
+            bool isValid = SteamUtils.GetImageSize(iImage, out uint width, out uint height);
+
+            if (isValid)
+            {
+                byte[] image = new byte[width * height * 4];
+
+                isValid = SteamUtils.GetImageRGBA(iImage, image, (int)(width * height * 4));
+
+                if (isValid)
+                {
+                    texture = new Texture2D((int)width, (int)height, TextureFormat.RGBA32, false, true);
+                    texture.LoadRawTextureData(image);
+                    texture.Apply();
+                }
+            }
+
+            return texture;
+        }
+
+        private void OnLobbyJoinRequested(GameLobbyJoinRequested_t callback)
+        {
+            Debug.Log($"Steam ID: {callback.m_steamIDLobby} is inviting us to their lobby");
+            SteamMatchmaking.JoinLobby(callback.m_steamIDLobby);
+        }
+
+        public void JoinLobby()
+        {
+            SteamMatchmaking.JoinLobby(new CSteamID(ulong.Parse(idInput.text)));
+        }
+
+        public void OnLobbyChatUpdate(LobbyChatUpdate_t callback)
+        {
+            switch (callback.m_rgfChatMemberStateChange)
+            {
+                case (uint)EChatMemberStateChange.k_EChatMemberStateChangeLeft:
+                    DisplayLobbyMembers(lobbyId);
+                    break;
+            }
+        }
+
+        public void LeaveLobby()
+        {
+            NetworkManager.Instance.Client.Disconnect();
+            SteamMatchmaking.LeaveLobby(lobbyId);
+            lobbyId = CSteamID.Nil;
+            idText.text = "Lobby ID: None";
+        }
+
+        public void BeginGame()
+        {
+            
+        }
     }
 }
--- country game/Assets/Scripts/CountryGame/Multiplayer/NetworkManager.cs
+++ country game/Assets/Scripts/CountryGame/Multiplayer/NetworkManager.cs
@@ -3,6 +3,14 @@ using Riptide;
 using Riptide.Transports.Steam;
 using Riptide.Utils;
 using Steamworks;
+using TMPro;
+using UnityEngine.UI;
+using SteamClient = Riptide.Transports.Steam.SteamClient;
+
+public enum LobbyMessageId : ushort
+{
+    BeginGame,
+}
 
 namespace CountryGame.Multiplayer
 {
@@ -35,6 +43,17 @@ namespace CountryGame.Multiplayer
         public const byte PlayerHostedDemoMessageHandlerGroupId = 255;
         
         public Server Server;
+        public Client Client;
+
+        [SerializeField] private Image profilePicture;
+        [SerializeField] private TextMeshProUGUI username;
+
+        [SerializeField] private GameObject mainScreen;
+        [SerializeField] private GameObject lobbyScreen;
+        [SerializeField] private GameObject gameScreen;
+
+        protected Callback<AvatarImageLoaded_t> AvatarImageLoaded;
+        //protected Callback<Lobby> LobbyEntered;
         
         private void Awake()
         {
@@ -50,11 +69,8 @@ namespace CountryGame.Multiplayer
             }
             
             RiptideLogger.Initialize(Debug.Log, Debug.Log, Debug.LogWarning, Debug.LogError, false);
-
-            SteamServer steamServer = new SteamServer();
-            Server = new Server(steamServer);
-            Server.ClientConnected += PlayerConnected;
-            Server.ClientDisconnected += PlayerDisconnected;
+            
+            OpenMainScreen();
         }
 
         private void Start()
@@ -64,34 +80,163 @@ namespace CountryGame.Multiplayer
                 Debug.LogError("Steam is not initialized!");
                 return;
             }
+            
+            SteamServer steamServer = new SteamServer();
+            Server = new Server(steamServer);
+            Server.ClientConnected += PlayerConnected;
+            Server.ClientDisconnected += PlayerDisconnected;
+
+            Client = new Client(new SteamClient(steamServer));
+            
+            Client.ClientConnected += ClientOnClientConnected;
+            Client.Connected += ClientOnClientConnected;
+            Client.ClientDisconnected += ClientOnClientDisconnected;
+            Client.Disconnected += ClientOnClientDisconnected;
+
+            AvatarImageLoaded = Callback<AvatarImageLoaded_t>.Create(OnAvatarImageLoaded);
+            
+            Debug.Log($"Logged in as {SteamFriends.GetPersonaName()}");
+            username.text = SteamFriends.GetPersonaName();
+            int imageId = SteamFriends.GetLargeFriendAvatar(SteamUser.GetSteamID());
+
+            if (imageId == -1)
+            {
+                return;
+            }
+
+            Texture2D texture = GetSteamImageAsTexture(imageId);
+
+            profilePicture.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
         }
 
-        private void Update()
+        private void ClientOnClientDisconnected(object sender, ClientDisconnectedEventArgs e)
+        {
+            //LobbyManager.Instance.DisplayLobbyMembers(LobbyManager.Instance.lobbyId);
+        }
+
+        private void ClientOnClientDisconnected(object sender, EventArgs e)
+        {
+            OpenMainScreen();
+        }
+
+        private void ClientOnClientConnected(object sender, ClientConnectedEventArgs e)
+        {
+            //LobbyManager.Instance.DisplayLobbyMembers(LobbyManager.Instance.lobbyId);
+        }
+
+        private void ClientOnClientConnected(object sender, EventArgs e)
+        {
+            OpenLobbyScreen();
+        }
+
+        public void OpenMainScreen()
+        {
+            mainScreen.SetActive(true);
+            lobbyScreen.SetActive(false);
+            gameScreen.SetActive(false);
+        }
+
+        public void OpenLobbyScreen()
+        {
+            mainScreen.SetActive(false);
+            lobbyScreen.SetActive(true);
+            gameScreen.SetActive(false);
+        }
+
+        public void OpenGameScreen()
+        {
+            mainScreen.SetActive(true);
+            lobbyScreen.SetActive(false);
+            gameScreen.SetActive(true);
+        }
+
+        protected void OnAvatarImageLoaded(AvatarImageLoaded_t callback)
+        {
+            if (callback.m_steamID == SteamUser.GetSteamID())
+            {
+                Texture2D texture = GetSteamImageAsTexture(callback.m_iImage);
+
+                profilePicture.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+            }
+        }
+
+        private Texture2D GetSteamImageAsTexture(int iImage)
+        {
+            Texture2D texture = null;
+
+            bool isValid = SteamUtils.GetImageSize(iImage, out uint width, out uint height);
+
+            if (isValid)
+            {
+                byte[] image = new byte[width * height * 4];
+
+                isValid = SteamUtils.GetImageRGBA(iImage, image, (int)(width * height * 4));
+
+                if (isValid)
+                {
+                    texture = new Texture2D((int)width, (int)height, TextureFormat.RGBA32, false, true);
+                    texture.LoadRawTextureData(image);
+                    texture.Apply();
+                }
+;            }
+
+            return texture;
+        }
+
+        private void FixedUpdate()
         {
             if (Server.IsRunning)
             {
                 Server.Update();
             }
+            
+            Client.Update();
         }
 
         private void OnApplicationQuit()
         {
             StopServer();
+            DisconnectClient();
             Server.ClientConnected -= PlayerConnected;
             Server.ClientDisconnected -= PlayerDisconnected;
+            Client.ClientConnected -= ClientOnClientConnected;
+            Client.Connected -= ClientOnClientConnected;
+            Client.ClientDisconnected -= ClientOnClientDisconnected;
+            Client.Disconnected -= ClientOnClientDisconnected;
+            SteamMatchmaking.LeaveLobby(SteamUser.GetSteamID());
         }
 
         internal void StopServer()
         {
             Server.Stop();
         }
+        
+        internal void DisconnectClient()
+        {
+            Client.Disconnect();
+        }
 
         private void PlayerConnected(object o, ServerConnectedEventArgs e)
         {
-            
+            Debug.Log("We connected from the lobby");
+            if (Server.ClientCount == 1)
+            {
+                Debug.Log("We are the first in the server");
+            }
         }
 
         private void PlayerDisconnected(object o, ServerDisconnectedEventArgs e)
+        {
+            Debug.Log("We disconnected from the lobby");
+        }
+
+        public void BeginGame()
+        {
+            OpenGameScreen();
+        }
+
+        [MessageHandler((ushort)LobbyMessageId.BeginGame, PlayerHostedDemoMessageHandlerGroupId)]
+        private void BeginGameMessageReceived(Message message, ushort fromClientId)
         {
             
         }
